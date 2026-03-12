5838e1282beac5f4823667c492c7d65a7dde85b5
NaduSharindi <nadusharindi@gmail.com>
2026-03-12 14:14:21 +0530

    Add FeelingCardGrid layout with grid-based feeling selection

--- ClinicFlow/Views/FeelingCardView.swift
+++ ClinicFlow/Views/FeelingCardView.swift
@@ -5,3 +5,28 @@
 //  Created by COBSCCOMP242P-063 on 2026-03-12.
 //
 
+import SwiftUI
+
+struct FeelingCardGrid: View {
+    @ObservedObject var vm: HomeViewModel
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 12) {
+            FeelingItem(label: "Good", icon: "face.smiling.fill", color: .green) {
+                vm.selectFeeling("Good")
+            }
+            FeelingItem(label: "Ok", icon: "face.dashed.fill", color: .blue) {
+                vm.selectFeeling("Ok")
+            }
+            FeelingItem(label: "Not Well", icon: "face.smiling.inverse", color: .orange) {
+                vm.selectFeeling("Not Well")
+            }
+            NavigationLink(destination: UrgentHelpPlaceholderView()) {
+                FeelingItem(label: "Urgent Help", icon: "exclamationmark.triangle.fill", color: .red){
+                    vm.selectFeeling("Urgent Help")
+                }
+            }
+        }
+    }
+}
