dc8bfb5210ba194e81a32c4c0224019078252547
NaduSharindi <nadusharindi@gmail.com>
2026-03-12 14:24:34 +0530

    Create NextAppointmentCard UI component

---
+++ ClinicFlow/Views/NextAppointmentCard.swift
@@ -0,0 +1,14 @@
+//
+//  NextAppointmentCard.swift
+//  ClinicFlow
+//
+//  Created by COBSCCOMP242P-063 on 2026-03-12.
+//
+
+import SwiftUI
+
+struct NextAppointmentCard: View {
+    let appointment: Appointment
+    let minutes: Int
+    
+    
